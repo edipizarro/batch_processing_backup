074fea8255f946b09397670b60bd0cfcd88aa77c
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2020-12-18 12:44:45 -0300

    read file with a specified format, works with s3

--- computing/get_magstats.py
+++ computing/get_magstats.py
@@ -16,8 +16,25 @@ from lc_correction.helpers import *
 @click.argument("node_id", type=str)
 @click.argument("job_id", type=str)
 @click.argument("logs_dir", type=str, default=".")
-@click.option("-v", "--version", default="bulk_version_0.0.1", help="Version of correction")
-def main(corrected_dir, non_det_dir, output_dir, partition, node_id, job_id, logs_dir, version):
+@click.option(
+    "-v", "--version", default="bulk_version_0.0.1", help="Version of correction"
+)
+@click.option(
+    "--file-format",
+    default="part-%s-d04f7f52-9773-4a13-a06b-55c7db613831-c000.snappy.parquet",
+    help="Parquet file name format. Id number should be replaced with %s. Default is part-%s-d04f7f52-9773-4a13-a06b-55c7db613831-c000.snappy.parquet",
+)
+def main(
+    corrected_dir,
+    non_det_dir,
+    output_dir,
+    partition,
+    node_id,
+    job_id,
+    logs_dir,
+    version,
+    file_format,
+):
     """
     Correct a set of detections indexed by object id.
 
@@ -33,10 +50,13 @@ def main(corrected_dir, non_det_dir, output_dir, partition, node_id, job_id, log
 
     JOB_ID identifier of job in slurm
     """
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    files = sorted(os.listdir(corrected_dir))
-    detection_file = files[partition]
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    detection_file = file_format % str(partition).zfill(5)
 
     logging.info(f"Opening corrected detection file: {detection_file}")
     detections = pd.read_parquet(os.path.join(corrected_dir, detection_file))
@@ -59,13 +79,16 @@ def main(corrected_dir, non_det_dir, output_dir, partition, node_id, job_id, log
 
     files = sorted(os.listdir(non_det_dir))
     non_det_file = files[partition]
-    non_det = pd.read_parquet(os.path.join(non_det_dir, non_det_file), columns=["objectId", "jd", "isdiffpos", "fid", "diffmaglim"])
+    non_det = pd.read_parquet(
+        os.path.join(non_det_dir, non_det_file),
+        columns=["objectId", "jd", "isdiffpos", "fid", "diffmaglim"],
+    )
     non_det["mjd"] = non_det.jd - 2400000.5
     del non_det["jd"]
 
     logging.info("Doing dm/dt")
     dd = do_dmdt_df(magstats, non_det)
-    magstats['step_id_corr'] = version
+    magstats["step_id_corr"] = version
 
     logging.info("Joining magstats and dm/dt")
     magstats = magstats.join(dd, on=["objectId", "fid"])
