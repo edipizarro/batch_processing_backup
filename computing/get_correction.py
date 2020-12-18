8246edef3bc4be54aacceccccf5d14be6240be04
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2020-12-18 15:29:10 -0300

    format strings

--- computing/get_correction.py
+++ computing/get_correction.py
@@ -23,6 +23,11 @@ from lc_correction.helpers import *
     default="part-%s-d04f7f52-9773-4a13-a06b-55c7db613831-c000.snappy.parquet",
     help="Parquet file name format. Id number should be replaced with %s. Default is part-%s-d04f7f52-9773-4a13-a06b-55c7db613831-c000.snappy.parquet",
 )
+@click.option(
+    "--format-times",
+    default=1,
+    help="Number of times the file_format has a string to be parsed. Example: 'my %s format %s' has value 2",
+)
 def get_correction(
     detections_dir,
     output_dir,
@@ -32,6 +37,7 @@ def get_correction(
     logs_dir,
     version,
     file_format,
+    format_times,
 ):
     """
     Correct a set of detections indexed by object id.
@@ -53,7 +59,16 @@ def get_correction(
         format="%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    detection_file = file_format % str(partition).zfill(5)
+
+    tup = (str(partition).zfill(5),)
+    if format_times > 1:
+        for i in range(format_times):
+            tup += tup
+
+    try:
+        detection_file = file_format % tup
+    except TypeError as e:
+        raise Exception("file_format and format_times not compatible")
 
     logging.info(f"Opening detection file: {detection_file}")
     detections = pd.read_parquet(os.path.join(detections_dir, detection_file))
