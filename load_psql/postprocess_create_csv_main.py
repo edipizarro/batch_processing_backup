322d1931f700474104991950a8024cd41a419d9e
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-04 18:11:41 -0300

    added cli command

--- load_psql/postprocess_create_csv_main.py
+++ load_psql/postprocess_create_csv_main.py
@@ -12,19 +12,25 @@ from load_psql.loaders import (
 
 from pyspark.sql import SparkSession, Window
 from pyspark import SparkConf, SparkContext
+import click
+import os
+import sys
 
 
 def create_session():
 
     # logging.info("Creating spark session")
     conf = SparkConf()
+    conf.set("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.2")
     spark = SparkSession.builder.config(conf=conf).getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
     return spark
 
 
-def get_tt_det():
-    pass
+def get_tt_det(session, det_src, raw_det_src):
+    df = session.read.load(det_src)
+    dfc = session.read.load(raw_det_src)
+    return df.alias("i").join(dfc.alias("c"), ["objectId", "candid"], "inner")
 
 
 def validate_config(config):
@@ -43,47 +49,66 @@ def validate_config(config):
     ]
     csv_loader_options = ["n_partitions", "max_records_per_file", "mode"]
     if "db" not in config:
-        return False
+        return False, "missing db key in config"
     if "tables" not in config:
-        return False
+        return False, "missing tables key in config"
     if "sources" not in config:
-        return False
+        return False, "missing sources key in config"
     if "outputs" not in config:
-        return False
+        return False, "missing outputs key in config"
     for table in all_tables:
         if table not in config["tables"]:
-            return False
+            return False, f"missing {table} in tables"
         if config["tables"][table] and table not in config["sources"]:
-            return False
+            return False, f"missing {table} in sources"
         if config["tables"][table] and table not in config["outputs"]:
-            return False
+            return False, f"missing {table} in outputs"
+    if "raw_detection" not in config["sources"]:
+        return False, "missing raw_detection in sources"
     if "csv_loader_config" not in config:
-        return False
+        return False, "missing csv_loader_config"
     for opt in csv_loader_options:
         if opt not in config["csv_loader_config"]:
-            return False
-    return True
+            return False, f"missing {opt} in config"
+    return True, None
 
 
-def create_and_upload_csv(Loader, table_name, config, session, default_args, kwargs):
+def create_and_upload_csv(Loader, table_name, config, session, default_args, **kwargs):
     loader = Loader(source=config["sources"][table_name], read_args=default_args)
     loader.save_csv(
         spark_session=session,
-        output_path=config["output"][table_name],
+        output_path=config["outputs"][table_name],
         n_partitions=config["csv_loader_config"]["n_partitions"],
         max_records_per_file=config["csv_loader_config"]["max_records_per_file"],
         mode=config["csv_loader_config"]["mode"],
-        **kwargs
+        **kwargs,
     )
     loader.psql_load_csv(config["outputs"][table_name], config["db"])
 
 
-def process_csv(config):
-    if not validate_config(config):
-        raise Exception("Invalid config")
+@click.command()
+@click.argument("config_file")
+@click.option(
+    "--log",
+    "loglevel",
+    default="INFO",
+    help="log level to use",
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]),
+)
+def process_csv(config_file, loglevel):
+    if not os.path.exists(config_file):
+        raise Exception("Config file not found")
+    sys.path.append(os.path.dirname(os.path.expanduser(config_file)))
+    from config import load_config as config
+
+    valid, message = validate_config(config)
+    if not valid:
+        raise Exception(message)
     spark = create_session()
     default_args = {}
-    tt_det = get_tt_det()
+    tt_det = get_tt_det(
+        spark, config["sources"]["detection"], config["sources"]["raw_detection"]
+    )
     step_id = "bulk_1.0.0"
     obj_cid_window = Window.partitionBy("objectId").orderBy("candid")
     if config["tables"]["detection"]:
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import click
 from partition_avro.partition_dets_ndets import partition_dets_ndets
-from partition_avro.postprocess_create_csv import create_csv
-from partition_avro.psql_load import load_csv
+from load_psql.postprocess_create_csv_main import process_csv
 
 
 @click.group()
@@ -11,6 +10,5 @@ def cli():
 
 if __name__ == "__main__":
     cli.add_command(partition_dets_ndets)
-    cli.add_command(create_csv)
-    cli.add_command(load_csv)
+    cli.add_command(process_csv)
     cli()
