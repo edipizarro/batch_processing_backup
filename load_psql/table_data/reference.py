947ee5915006e8e95909ad5aca997e5503c2c87b
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-13 11:31:00 -0300

    added fillna

--- load_psql/table_data/reference.py
+++ load_psql/table_data/reference.py
@@ -39,7 +39,14 @@ class ReferenceTableData(TableData):
             .withColumnRenamed("i.jdstartref", "mjdstartref")
             .withColumnRenamed("i.jdendref", "mjdendref")
             .where(col("candid") == col("auxcandid"))
-            .fillna({"rfid": -9999})
+            .fillna(
+                {
+                    "rfid": -9999,
+                    "mjdstartref": -9999,
+                    "mjdendref": -9999,
+                    "nframesref": -9999,
+                }
+            )
             .select(
                 *[
                     col(c).cast(IntegerType())
