5dd8fb4a7b852edcdba0afb52a87169fa04eb5e0
Camilo Valenzuela <cacke91@gmail.com>
2021-01-15 11:12:36 -0300

    Reorder xmatch

--- load_psql/table_data/xmatch.py
+++ load_psql/table_data/xmatch.py
@@ -1,19 +1,22 @@
 from .generic import TableData
 from pyspark.sql.functions import col, lit
 
+#oid, catid, oid_catalog,dist
 
 class XmatchTableData(TableData):
     def select(self, column_list: list):
+        column_list.remove("objectId_2")
         column_list.remove("catid")
 
         sel_xmatch = (
             self.dataframe.select(
+                col("objectId_2"),
+                lit("allwise").alias("catid"),
                 *[col(c) for c in column_list],
             )
             .withColumnRenamed("designation", "oid_catalog")
             .withColumnRenamed("objectId_2", "oid")
             .withColumnRenamed("distance", "dist")
-            .withColumn("step_id_corr", lit("allwise"))
         )
 
         return sel_xmatch
