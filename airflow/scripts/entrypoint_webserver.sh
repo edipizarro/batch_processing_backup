32461ac9e456b1109f388ca53e6cb9ddc76f6684
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-15 17:09:24 -0300

    added templates to use multi command sshoperator

--- airflow/scripts/entrypoint_webserver.sh
+++ airflow/scripts/entrypoint_webserver.sh
@@ -3,4 +3,5 @@ airflow users create -u $ADMIN_USER -r Admin -p $ADMIN_PASSWORD -f admin -l aler
 airflow variables import /opt/airflow/variables/variables.json
 airflow connections add reuna_connection --conn-description="connect to reuna through ssh" --conn-host="alerce.reuna.cl" --conn-port="22" --conn-type="ssh"
 airflow connections add reuna_ftp_connection --conn-description="SFTP connection for reuna" --conn-host="alerce.reuna.cl" --conn-port="22" --conn-type="sftp"
+airflow connections add ztf_db --conn-description="ZTF DB Connection" --conn-type="postgres"
 airflow webserver
