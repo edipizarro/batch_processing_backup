e73d257cba7408637af9d72d00c991ff3b2e9b83
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-18 17:45:55 -0300

    remove aws credentials env vars

--- airflow/templates/reuna_create_csv_template.sh
+++ airflow/templates/reuna_create_csv_template.sh
@@ -2,6 +2,4 @@ cd batch_processing
 {% if params.virtualenv %}
 conda activate {{params.virtualenv}}
 {% endif %}
-export AWS_ACCESS_KEY_ID={{params.aws_access_key}}
-export AWS_SECRET_ACCESS_KEY={{params.aws_secret_access_key}}
-AWS_ACCESS_KEY_ID={{params.aws_access_key}} AWS_SECRET_ACCESS_KEY={{params.aws_secret_access_key}} python  main.py create-csv --config_json='{{params.vars}}'
+python  main.py create-csv --config_json='{{params.vars}}'
