8478591fb5e38a4f5b86115ddda318bea4d57e3f
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-18 17:29:48 -0300

    force env variables

--- airflow/templates/reuna_create_csv_template.sh
+++ airflow/templates/reuna_create_csv_template.sh
@@ -1,7 +1,7 @@
+cd batch_processing
 {% if params.virtualenv %}
 conda activate {{params.virtualenv}}
 {% endif %}
 export AWS_ACCESS_KEY_ID={{params.aws_access_key}}
 export AWS_SECRET_ACCESS_KEY={{params.aws_secret_access_key}}
-cd batch_processing
-python main.py create-csv --config_json='{{params.vars}}'
+AWS_ACCESS_KEY_ID={{params.aws_access_key}} AWS_SECRET_ACCESS_KEY={{params.aws_secret_access_key}} python  main.py create-csv --config_json='{{params.vars}}'
