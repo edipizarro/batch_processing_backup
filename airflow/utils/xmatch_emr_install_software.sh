098a8c0c6dde1abaa8d4809150fc3d5432a876f8
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-20 14:04:56 -0300

    fixed bootstrap

--- airflow/utils/xmatch_emr_install_software.sh
+++ airflow/utils/xmatch_emr_install_software.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -e
 
 # Install git
 sudo yum install -y git
@@ -10,23 +10,20 @@ sudo python3 -m pip install ipython
 # Add pyspark env variables
 echo "export PYSPARK_DRIVER_PYTHON=ipython3" >> $HOME/.bashrc
 echo "export PYSPARK_PYTHON=/usr/bin/python3.6" >> $HOME/.bashrc
+source $HOME/.bashrc
 # Clone and install required packages for astroide
 git clone https://github.com/alercebroker/minimal_astroide.git /tmp/minimal_astroide
-cd /tmp/minimal_astroide
 ## Install gradle
-echo "Wget gradle"
 wget https://services.gradle.org/distributions/gradle-4.4.1-bin.zip -O /tmp/gradle.zip
-echo "Unzip gradle"
 sudo unzip -d /opt /tmp/gradle.zip
-echo "Unzipped /tmp/gradle.zip to /opt"
 ls /opt
 sudo ln -s /opt/gradle-4.4.1 /opt/gradle
-echo "export GRADLE_HOME=/opt/gradle" >> $HOME/.bashrc
-echo "export PATH=\$PATH:/opt/gradle/bin" >> $HOME/.bashrc
-source $HOME/.bashrc
+export GRADLE_HOME=/opt/gradle
+export PATH=$PATH:/opt/gradle/bin
 gradle -v
+cd /tmp/minimal_astroide
 gradle build
-cp build/minimal_astroide.jar /tmp
-cp libs/healpix-1.0.jar /tmp
+cp /tmp/minimal_astroide/build/libs/minimal_astroide.jar /tmp
+cp /tmp/minimal_astroide/libs/healpix-1.0.jar /tmp
 sudo python3 /tmp/minimal_astroide/python_wrapper/setup.py install
 
