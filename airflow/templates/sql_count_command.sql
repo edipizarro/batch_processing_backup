ba1ee4632b4c8d39b1908b1cfcdd74112695482b
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-26 17:40:50 -0300

    fixing template

--- airflow/templates/sql_count_command.sql
+++ airflow/templates/sql_count_command.sql
@@ -1,4 +1,4 @@
-{% set xcom=ti.xcom_pull(task_ids='count_csv_tuples_{{params.table}}') %}
+{% set xcom=ti.xcom_pull(task_ids=f'count_csv_tuples_{params["table"]}') %}
 
 select count(*) from {{params.table}}
 having count(*) = {% if xcom -%} {{xcom.strip().decode()}} {% endif %}
