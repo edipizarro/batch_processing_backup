3e383a66ac687108ae3a13a0cf3c86a3e0f93e02
Ubuntu <ubuntu@ip-172-31-90-247.ec2.internal>
2021-01-26 21:45:56 +0000

    fixed template

--- airflow/templates/reuna_process_csv_template.sh
+++ airflow/templates/reuna_process_csv_template.sh
@@ -2,6 +2,6 @@ cd batch_processing
 {% if {{params.virtualenv}} -%}
 conda activate {{params.virtualenv}}
 {% endif %}
-nohup python main.py process-csv --config_json='{{params.vars}}' &
+python main.py process-csv --config_json='{{params.vars}}'
 
 
