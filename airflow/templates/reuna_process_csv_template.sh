0f552ce789334acafc1a5614c572956599b2ed6d
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-01-27 10:44:27 -0300

    fix conditional in template

--- airflow/templates/reuna_process_csv_template.sh
+++ airflow/templates/reuna_process_csv_template.sh
@@ -1,5 +1,5 @@
 cd batch_processing
-{% if {{params.virtualenv}} -%}
+{% if params.virtualenv -%}
 conda activate {{params.virtualenv}}
 {% endif %}
 python main.py process-csv --config_json='{{params.vars}}'
