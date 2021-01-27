895ab376ec79a1836c7c729853053a69040db67b
Camilo Valenzuela <cacke91@gmail.com>
2021-01-27 12:21:02 -0300

    Added double to probability value type

--- load_psql/table_data/probability.py
+++ load_psql/table_data/probability.py
@@ -1,11 +1,12 @@
-from .generic import TableData
 from pyspark.sql.functions import explode, col, struct, lit, array, split, dense_rank, desc
 from pyspark.sql import Window
+
+from .generic import TableData
 
 class ProbabilityTableData(TableData):
     def select(self, column_list, version, classifier_name):
         cols_p = [
-                c for (c, t) in self.dataframe.dtypes if (c not in ["oid"] and t == 'float')  
+                c for (c, t) in self.dataframe.dtypes if (c not in ["oid"] and t in ['float', 'double'])  
         ]
         new_cols = explode(array([struct(lit(c).alias("key"), col(c).alias("value")) for c in cols_p])).alias("new_cols")
         df = (
@@ -20,6 +21,4 @@ class ProbabilityTableData(TableData):
         df = df.withColumn("ranking", dense_rank().over(Window.partitionBy("oid").orderBy(desc("probability"))))
         df = df.drop("key").drop("value")
         sel_prob = df.select(column_list)
-        return sel_prob
-
-
+        return sel_prop
