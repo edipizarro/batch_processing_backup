c2f4756928dd2e740ff18883afec811912ce929c
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-07-15 12:45:32 -0400

    installing postgresql-devel

--- airflow/utils/emr_install_software.sh
+++ airflow/utils/emr_install_software.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo yum install -y git
+sudo yum install -y postgresql-devel
 git clone https://github.com/alercebroker/batch_processing.git /tmp/batch_processing
-sudo python3 -m pip install -r /tmp/batch_processing/requirements.txt
-sudo python3 -m pip install ipython
 echo "export PYSPARK_DRIVER_PYTHON=ipython3" >> $HOME/.bashrc
 echo "export PYSPARK_PYTHON=/usr/bin/python3.6" >> $HOME/.bashrc
 source $HOME/.bashrc
+sudo python3 -m pip install -r /tmp/batch_processing/requirements.txt
+sudo python3 -m pip install ipython
