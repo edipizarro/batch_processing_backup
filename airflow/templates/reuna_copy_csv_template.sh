bbb77509bd01e95ab50192f58becff7fc9a66b3f
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-07-16 14:17:25 -0400

    run python3

--- airflow/templates/reuna_copy_csv_template.sh
+++ airflow/templates/reuna_copy_csv_template.sh
@@ -1,5 +1,8 @@
 cd batch_processing
 {% if params.virtualenv %}
 conda activate {{params.virtualenv}}
-{% endif %}
 python main.py psql-copy-csv --config_json='{{params.vars}}'
+{% endif %}
+{% if not params.virtualenv %}
+python3 main.py psql-copy-csv --config_json='{{params.vars}}'
+{% endif %}
