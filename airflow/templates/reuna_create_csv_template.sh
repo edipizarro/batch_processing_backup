0f191e449964785594cf82fa317f9e835bd516ef
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-07-16 14:17:25 -0400

    run python3
    Former-commit-id: d92d7d2dfeae02418b76a8f2e7915bcc47050fd3

--- airflow/templates/reuna_create_csv_template.sh
+++ airflow/templates/reuna_create_csv_template.sh
@@ -1,5 +1,8 @@
 cd batch_processing
 {% if params.virtualenv %}
 conda activate {{params.virtualenv}}
-{% endif %}
 python main.py create-csv --config_json='{{params.vars}}'
+{% endif %}
+{% if not params.virtualenv %}
+python3 main.py create-csv --config_json='{{params.vars}}'
+{% endif %}
--- airflow/templates/reuna_process_csv_template.sh
+++ airflow/templates/reuna_process_csv_template.sh
@@ -1,7 +1,8 @@
 cd batch_processing
 {% if params.virtualenv -%}
 conda activate {{params.virtualenv}}
-{% endif %}
 python main.py process-csv --config_json='{{params.vars}}'
-
-
+{% endif %}
+{% if not params.virtualenv -%}
+python3 main.py process-csv --config_json='{{params.vars}}'
+{% endif %}
