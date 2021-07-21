3cfe4e05f8b3a963a5fa68585592897b25b96fff
Javier <javier.arredondo.c@usach.cl>
2021-07-21 12:27:57 -0400

    changed script for get probs. only do inference
    Former-commit-id: 3ffca295fee93dd7fc0208c120d0c830b3e267f7 [formerly 0f4291ff491b3ecdd2c9dad4a068b1f383776ad6]
    Former-commit-id: b8aa8a77a851d04c6e2c5b08faf9f8ed5ee12ec7

--- computing/get_lc_classifier_probs.py
+++ computing/get_lc_classifier_probs.py
@@ -1,36 +1,9 @@
-import dask.dataframe as dd
 import click
 import logging
 import os
 
 import pandas as pd
 from lc_classifier.classifier.models import HierarchicalRandomForest
-
-
-def compute_features(features_dir, xmatches_dir):
-    features = dd.read_parquet(os.path.join(features_dir, "*.parquet")).compute()
-    logging.info(f"Features opened ({features.shape[0]} objects with features)")
-
-    if os.path.isdir(xmatches_dir):
-        xmatches = dd.read_parquet(os.path.join(xmatches_dir, "*.parquet")).compute()
-    else:
-        xmatches = pd.read_parquet(xmatches_dir)
-    logging.info(f"Xmatches opened ({xmatches.shape[0]} objects with xmatch)")
-
-    logging.info("Joining data")
-    features = features.join(xmatches.set_index("objectId_2"), on="oid")
-
-    logging.info("Getting new features from xmatch")
-    features["Period_fit"] = features["PPE"]
-    features["W1-W2"] = features["w1mpro"] - features["w2mpro"]
-    features["W2-W3"] = features["w2mpro"] - features["w3mpro"]
-    features["g-W2"] = features["mean_mag_1"] - features["w2mpro"]
-    features["g-W3"] = features["mean_mag_1"] - features["w3mpro"]
-    features["r-W2"] = features["mean_mag_2"] - features["w2mpro"]
-    features["r-W3"] = features["mean_mag_2"] - features["w3mpro"]
-
-    features.set_index("oid", inplace=True)
-    return features
 
 
 def compute_probabilities(features, output_path=None):
@@ -65,14 +38,19 @@ def compute_probabilities(features, output_path=None):
 
 @click.command()
 @click.argument("features_dir", type=str)
-@click.argument("xmatches_dir", type=str)
 @click.argument("output_path", type=str)
-def get_probs(features_dir, xmatches_dir, output_path):
+def get_probs(features_dir, output_path):
     logging.basicConfig(level="INFO",
                         format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    features = compute_features(features_dir, xmatches_dir)
+    features = pd.read_parquet(features_dir)
+    if features.index.name != "oid":
+        if "oid" in features.columns:
+            features.set_index("oid", inplace=True)
+        else:
+            raise Exception("Fatal error: We need the 'oid' column or 'oid' index")
+    features = features.astype(float)
 
     if not os.path.isdir(output_path):
         os.mkdir(output_path)
