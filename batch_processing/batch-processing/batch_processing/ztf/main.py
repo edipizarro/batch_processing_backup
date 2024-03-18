51107fcfcab93977c9ed5b98769072474b8b281d
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2024-03-18 10:52:34 -0300

    chore: change ztf file name
    Former-commit-id: 5909966860e8959cd94a1541e5e0e0868f1d03b8

--- batch_processing/batch-processing/batch_processing/ztf/main.py
+++ batch_processing/batch-processing/batch_processing/ztf/main.py
@@ -2,31 +2,46 @@ import json
 import requests
 import tarfile
 import os
-import shutil
 from datetime import datetime
+import tarfile
 
 import fastavro
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.types import StructType
 
-from .utils import Utils
+from batch_utils import (
+    date_to_mjd,
+    str_to_date,
+    dates_between_generator,
+    configure_logger,
+    _init_path,
+    _rm_directory_or_file,
+)
 
-class ZTFUtils():
+from .spark_schemas import avro_schema_alert
+
+class ZTFCrawler():
     """
-    ZTFUtils class for handling ZTF data processing tasks.
+    ZTFCrawler class for handling ZTF data processing tasks.
     """
-    def __init__(self, config_path: str):
+    def __init__(self, config_path: str = None, config_dict: dict = None):
         """
-        Initialize the ZTFUtils class.
+        Initialize the ZTFCrawler class.
 
         :param config_path: Path to the configuration file in JSON format.
         """
-        # Load json config
-        with open(config_path) as config:
-            self.config = json.loads(config.read())
+        if config_path and config_dict:
+            raise TypeError("config_path or config_dict should be given, not both")
+        elif config_path: # Load json config
+            with open(config_path) as config:
+                self.config = json.loads(config.read())
+        elif config_dict:
+            self.config = config_dict
+        else:
+            raise TypeError("config_path or config_dict should be given")
 
         # Configure logger
-        self.logger = Utils.configure_logger()
+        self.logger = configure_logger()
 
         # Configure Spark
         self.spark = None
@@ -39,18 +54,6 @@ class ZTFUtils():
         """
         self.spark = spark
 
-    
-    def get_downloads_base_folder(self) -> str:
-        """
-        Get the base path where downloaded files will be saved.
-
-        :return: Base path of downloads.
-        """
-        return os.path.join(
-            self.config["files_base_folder"],
-            self.config["downloads_folder"]
-        )
-
     def get_downloads_mjd_folder(self, mjd) -> str:
         """
         Get the path where downloaded files for a specific MJD will be saved.
@@ -59,9 +62,9 @@ class ZTFUtils():
         :return: Base path of downloads for the specified MJD.
         """
         return os.path.join(
-            self.config["files_base_folder"],
-            self.config["downloads_folder"],
-            mjd + self.config["url_source_file_postfix"]
+            self.config["DataFolder"],
+            self.config["SubDataFolder"]["CompressedAvros"],
+            mjd + self.config["UrlSourceFilePostfix"]
         )
     
     def get_uncompressed_mjd_folder(self, mjd) -> str:
@@ -72,8 +75,8 @@ class ZTFUtils():
         :return: Base path for uncompressed files of the specified MJD.
         """
         return os.path.join(
-            self.config["files_base_folder"],
-            self.config["untar_folder"],
+            self.config["DataFolder"],
+            self.config["SubDataFolder"]["UncompressedAvros"],
             mjd
         )
 
@@ -85,8 +88,8 @@ class ZTFUtils():
         :return: Path to the Parquet folder for the specified MJD.
         """
         return os.path.join(
-            self.config["files_base_folder"],
-            self.config["parquet_folder"],
+            self.config["DataFolder"],
+            self.config["SubDataFolder"]["RawParquet"],
             mjd
         )
 
@@ -103,12 +106,12 @@ class ZTFUtils():
         Remove a directory or file based on the specified case.
 
         :param path: Path to the directory or file.
-        :param case: String representing the case (e.g., "overwrite_data").
+        :param case: String representing the case (e.g., "OverwriteData").
         """
         if ((self._exists_directory_or_file(path))
             and self.config[case] in [True, "True", "true", "T", "t"]):
                 self.logger.info(f"Deleting existing files at {path}.")
-                Utils._rm_directory_or_file(path)
+                _rm_directory_or_file(path)
 
     def _download_file(self, url: str, path: str):
         """
@@ -117,7 +120,7 @@ class ZTFUtils():
         :param url: URL of the file to be downloaded.
         :param path: Path where the downloaded file will be saved.
         """
-        self._rm_directory_or_file(path, "overwrite_data")
+        self._rm_directory_or_file(path, "OverwriteData")
 
         if self._exists_directory_or_file(path) :
             self.logger.info(f"{path} already exists")
@@ -154,13 +157,20 @@ class ZTFUtils():
         """
         ztf_date = date.strftime("%Y%m%d")
         url = os.path.join(
-            self.config["url_source_base"],
-            self.config["url_source_file_prefix"] + ztf_date + self.config["url_source_file_postfix"]
+            self.config["UrlSourceBase"],
+            self.config["UrlSourceFilePrefix"] + ztf_date + self.config["UrlSourceFilePostfix"]
         )
-        directory = self.get_downloads_base_folder()
-        Utils._init_path(directory)
-        path = os.path.join(directory, mjd + self.config["url_source_file_postfix"])
-        self._download_file(url, path)
+        download_directory = os.path.join(
+            self.config["DataFolder"],
+            self.config["SubDataFolder"]["CompressedAvros"]
+        )
+
+        _init_path(download_directory)
+        download_path = os.path.join(
+            download_directory,
+            mjd + self.config["UrlSourceFilePostfix"]
+        )
+        self._download_file(url, download_path)
     
     def _untar_file(self, source: str, destination_folder: str, progress = False):
         """
@@ -170,7 +180,7 @@ class ZTFUtils():
         :param destination_folder: Folder where the content of the tar file will be extracted.
         :param progress: If True, shows progress and extracts by member (slower).
         """
-        self._rm_directory_or_file(destination_folder, "overwrite_data")
+        self._rm_directory_or_file(destination_folder, "OverwriteData")
         file_size = os.stat(source).st_size/(1024**2)
         self.logger.info(f"Opening {source} with size {file_size:.2f} MB")
 
@@ -178,7 +188,7 @@ class ZTFUtils():
             self.logger.info(f"{destination_folder} already exists")
             return
         
-        Utils._init_path(destination_folder)
+        _init_path(destination_folder)
         with tarfile.open(source, 'r') as tar:
             if not progress:
                 tar.extractall(destination_folder, filter="data")
@@ -194,7 +204,7 @@ class ZTFUtils():
                     progress = (extracted_members / total_members) * 100
                     self.logger.info(f"Extraction Progress: {progress:.2f}% | Currently extracting: {member.name}")
 
-        self._rm_directory_or_file(source, "delete_data")
+        self._rm_directory_or_file(source, "DeleteData")
         self.logger.info("Extraction complete!")
 
     def _untar_ztf_public(self, mjd: str, progress = False):
@@ -234,34 +244,28 @@ class ZTFUtils():
         avro_paths = [os.path.join(directory, avro) for avro in os.listdir(directory)]
         return self._get_avro_generator(avro_paths, avro_schema)
 
-    def _infer_schema(self, example_dict: dict) -> StructType:
-        """
-        Infer the schema of a PySpark DataFrame from an example dictionary.
-
-        :param example_dict (dict): An example dictionary.
-        :return StructType: The inferred PySpark StructType schema.
-        """
-        fields = [StructField(key, StringType(), nullable=True) for key in example_dict.keys()]
-        return StructType(fields)
-
-    def _write_batch_to_parquet(self, batch: list[dict], schema: StructType, output_folder: str, batch_id: int):
+    def _write_batch_to_parquet(self, batch: list[dict], schema: StructType, output_folder: str, name: str):
         """
         Write a batch of dictionaries to a Parquet file using PySpark.
 
         :param batch: List of dictionaries.
         :param schema: PySpark StructType schema.
         :param output_folder: Folder where the Parquet file will be saved.
-        :param batch_id: Param used to identify the parquet file.
+        :param name: Param used to identify the parquet file.
         """
         # Convert the batch to a PySpark DataFrame
         df = self.spark.createDataFrame(batch, schema)
 
         # Generate a unique filename based on timestamp or other criteria
-        parquet_filename = f"{batch_id}"
+        parquet_filename = f"{name}"
 
         # Write the PySpark DataFrame to a Parquet file
         parquet_path = os.path.join(output_folder, parquet_filename)
-        df.write.parquet(parquet_path, mode="ignore")
+        df.write.parquet(
+            parquet_path,
+            mode="ignore",
+            compression=self.config["ParquetCompression"]
+        )
 
         self.logger.info(f"Parquet file written: {parquet_path}")
 
@@ -274,33 +278,40 @@ class ZTFUtils():
         :param output_folder: Folder where the Parquet files will be saved.
         """
 
-        self._rm_directory_or_file(output_folder, "overwrite_data")
-        Utils._init_path(output_folder)
+        self._rm_directory_or_file(output_folder, "OverwriteData")
+        _init_path(output_folder)
 
-        try:
-            first = next(avro_generator)
-        except StopIteration:
-            self.logger.info("Current MJD is empty")
-            return
-        schema = self._infer_schema(first)
+        if batch_size == 0:
+            batch = list(avro_generator)
+            schema = avro_schema_alert
+            self._write_batch_to_parquet(batch, schema, output_folder, "0")
+        elif batch_size > 0:
+            try:
+                first = next(avro_generator)
+            except StopIteration:
+                self.logger.info("Current MJD is empty")
+                return
+            schema = avro_schema_alert
 
-        batch = [first]
-        avros_in_batch = 1
-        current_batch = 0
+            batch = [first]
+            avros_in_batch = 1
+            current_batch = 0
 
-        for avro_dict in avro_generator:
-            batch.append(avro_dict)
-            avros_in_batch += 1
+            for avro_dict in avro_generator:
+                batch.append(avro_dict)
+                avros_in_batch += 1
 
-            if avros_in_batch == batch_size:
+                if avros_in_batch == batch_size:
+                    self._write_batch_to_parquet(batch, schema, output_folder, current_batch)
+                    batch = []
+                    avros_in_batch = 0
+                    current_batch += 1
+
+            # Write the remaining items as the last batch (if any)
+            if batch:
                 self._write_batch_to_parquet(batch, schema, output_folder, current_batch)
-                batch = []
-                avros_in_batch = 0
-                current_batch += 1
-
-        # Write the remaining items as the last batch (if any)
-        if batch:
-            self._write_batch_to_parquet(batch, schema, output_folder, current_batch)
+        else:
+            raise ValueError("batch_size must be a number greater or equal to 0")
     
     def _create_ztf_public_parquet(self, mjd: str):
         """
@@ -310,20 +321,24 @@ class ZTFUtils():
         """
         avro_generator = self._get_ztf_public_avro_generator(mjd, avro_schema=None)
         directory = self.get_parquet_mjd_folder(mjd)
-        self._create_parquet_files(avro_generator, self.config["batch_size"], directory)
+        self._create_parquet_files(avro_generator, self.config["BatchSize"], directory)
         
         avros_directory = self.get_uncompressed_mjd_folder(mjd)
-        self._rm_directory_or_file(avros_directory, "delete_data")
+        self._rm_directory_or_file(avros_directory, "DeleteData")
     
     def execute(self):
         """
         Execute the ZTF data processing pipeline.
         """
-        date_format = self.config["date_format"]
-        start_date = Utils.str_to_date(self.config["start_date"], date_format)
-        end_date = Utils.str_to_date(self.config["end_date"], date_format)
-        for date in Utils.dates_between_generator(start_date, end_date):
-            mjd = Utils.date_to_mjd(date)
+        DateFormat = self.config["DateFormat"]
+        StartDate = str_to_date(self.config["StartDate"], DateFormat)
+        EndDate = str_to_date(self.config["EndDate"], DateFormat)
+        for date in dates_between_generator(StartDate, EndDate):
+            mjd = date_to_mjd(date)
             self._download_ztf_public(date, mjd)
-            self._untar_ztf_public(mjd)
+            try:
+                self._untar_ztf_public(mjd)
+            except tarfile.ReadError as e:
+                self.logger.warn(f"NOT VALID TAR FOR {mjd}")
+                continue
             self._create_ztf_public_parquet(mjd)
