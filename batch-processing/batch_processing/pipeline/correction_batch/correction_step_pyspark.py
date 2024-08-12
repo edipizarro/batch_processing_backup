fix small calculation issues
Former-commit-id: e5cef182050bd725364bae50a469144132f8db5f

batch-processing/batch_processing/pipeline/correction_batch/correction_step_pyspark.py
@@ -161,29 +161,49 @@ def correct(corrector_detections):
     return corrector_detections
 
 def restruct_extrafields(corrector_detections):
-    columns_not_extrafields = ['aid', 'candid', 'corrected', 'dec', 'dubious', 'e_dec', 'e_mag', 'e_mag_corr', 'e_mag_corr_ext', 'e_ra', 'fid', 'forced', 'has_stamp', 'isdiffpos', 'mag', 'mag_corr', 'mjd', 'oid', 'parent_candid', 'pid', 'ra', 'sid', 'stellar', 'tid', 'unparsed_fid', 'unparsed_isdiffpos', 'unparsed_jd']
+    columns_not_extrafields = ['aid', 'candid', 'corrected', 'dec', 'dubious', 'e_dec', 'e_mag', 'e_mag_corr', 'e_mag_corr_ext', 'e_ra', 'fid', 'forced', 'has_stamp', 'isdiffpos', 'mag', 'mag_corr', 'mjd', 'oid', 'parent_candid', 'pid', 'ra', 'sid', 'stellar', 'tid', 'unparsed_fid', 'unparsed_isdiffpos', 'unparsed_jd', 'diffmaglim', 'nid', 'magap', 'sigmagap', 'distnr', 'rb', 'rbversion', 'magapbig', 'sigmagapbig', 'rfid' ]
     columns_to_nest = [col for col in corrector_detections.columns if col not in columns_not_extrafields]
     nested_col = struct(*columns_to_nest)
     corrector_detections = corrector_detections.select('*', nested_col.alias('extra_fields'))
-    corrector_detections = corrector_detections.select('aid', 'candid', 'corrected', 'dec', 'dubious', 'e_dec', 'e_mag', 'e_mag_corr', 'e_mag_corr_ext', 'e_ra', 'extra_fields', 'fid', 'forced', 'has_stamp', 'isdiffpos', 'mag', 'mag_corr', 'mjd', 'oid', 'parent_candid', 'pid', 'ra', 'sid', 'stellar', 'tid', 'unparsed_fid', 'unparsed_isdiffpos', 'unparsed_jd')
+    corrector_detections = corrector_detections.select('aid', 'candid', 'corrected', 'dec', 'diffmaglim', 'distnr', 'dubious', 'e_dec', 'e_mag', 'e_mag_corr', 'e_mag_corr_ext', 'e_ra', 'extra_fields', 'fid', 'forced', 'has_stamp', 'isdiffpos', 'mag', 'mag_corr', 'magap', 'magapbig', 'mjd', 'nid', 'oid', 'parent_candid', 'pid', 'ra', 'rb', 'rbversion', 'rfid', 'sid', 'sigmagap', 'sigmagapbig', 'stellar', 'tid', 'unparsed_fid', 'unparsed_isdiffpos', 'unparsed_jd')
     return corrector_detections
 
-# Add a column to the dataframe, corresponding to the transformed e_ra and e_dec to arcsec
-def arcsec2dec_era_edec(non_forced):
-    non_forced = non_forced.withColumn("e_ra_arcsec", col("e_ra") / 3600.0)\
-                           .withColumn("e_dec_arcsec", col("e_dec") / 3600.0)
-    return non_forced
-
-def create_weighted_columns(non_forced):
-    non_forced = non_forced.withColumn("weighted_e_ra", pow(col("e_ra_arcsec"), -2.0).cast('float'))\
-                           .withColumn("weighted_e_dec", pow(col("e_dec_arcsec"), -2.0).cast('float'))
-    non_forced.filter(col('oid')=='ZTF22abuviyj').show()
-    return non_forced
 
 def get_non_forced(corrector_detections):
     corrector_detections = corrector_detections.filter(~col("forced"))
     return corrector_detections
 
+
+def arcsec2dec_era_edec(non_forced):
+    non_forced = non_forced.withColumn("e_ra_arcsec", col("e_ra") / 3600.0)
+    non_forced = non_forced.withColumn("e_dec_arcsec", col("e_dec") / 3600.0)
+    return non_forced
+
+def create_weighted_columns(non_forced):
+    non_forced = non_forced.withColumn("weighted_e_ra", 1.0 / (col("e_ra_arcsec") ** 2.0))
+    non_forced = non_forced.withColumn("weighted_e_dec", 1.0 / (col("e_dec_arcsec") ** 2.0))
+    return non_forced
+
+def correct_coordinates(non_forced):
+    non_forced = non_forced.repartition("oid")
+    non_forced = arcsec2dec_era_edec(non_forced)
+    non_forced = create_weighted_columns(non_forced)
+    window_spec = Window.partitionBy("oid")
+
+    non_forced = non_forced.withColumn("weighted_sum_ra", F.sum(F.col("ra") * F.col("weighted_e_ra")).over(window_spec)) \
+                           .withColumn("weighted_sum_dec", F.sum(F.col("dec") * F.col("weighted_e_dec")).over(window_spec)) \
+                           .withColumn("total_weight_e_ra", F.sum("weighted_e_ra").over(window_spec)) \
+                           .withColumn("total_weight_e_dec", F.sum("weighted_e_dec").over(window_spec))
+    
+    corrected_coords = non_forced.withColumn("meanra", col("weighted_sum_ra") / col("total_weight_e_ra")) \
+                                       .withColumn("sigmara", 3600.0 * sqrt(1 / col("total_weight_e_ra"))) \
+                                       .withColumn("meandec", col("weighted_sum_dec") / col("total_weight_e_dec")) \
+                                       .withColumn("sigmadec", 3600.0 * sqrt(1 / col("total_weight_e_dec")))
+    
+    corrected_coords = corrected_coords.select("oid", "meanra", "meandec")
+    return corrected_coords
+
+"""
 def correct_coordinates(non_forced):
     # Calculate weighted sum of RA and weighted sum of Dec
     weighted_coords = non_forced.groupBy("oid").agg(
@@ -199,7 +219,7 @@ def correct_coordinates(non_forced):
                                       .select("oid", "meanra", "meandec")
 
     return corrected_coords
-
+"""
 
 def execute_corrector(lightcurve_df):
     detections, non_detections, candids = separate_dataframe_lc_df(lightcurve_df)
@@ -228,6 +248,8 @@ def execute_corrector(lightcurve_df):
     non_detections = non_detections.drop_duplicates(["oid", "mjd", "fid"])
     sorted_columns_nondetections = sorted(non_detections.columns)
     non_detections = non_detections.select(*sorted_columns_nondetections)
+    corrector_detections.filter(col('oid')=='ZTF18aaeixba').show()
+    corrected_coordinates.filter(col('oid')=='ZTF19abzlyqu').show(truncate=False)
     return corrector_detections, corrected_coordinates, non_detections, candids
 
 

batch-processing/batch_processing/pipeline/sorting_hat/sorting_hat_spark.py
@@ -1,5 +1,6 @@
 from ..spark_init.pyspark_configs import *
 
+
 schema_avros = StructType([StructField('schemavsn', StringType(), True), StructField('publisher', StringType(), True), StructField('objectId', StringType(), True), StructField('candidate', StructType([StructField('jd', DoubleType(), True), StructField('fid', LongType(), True), StructField('pid', LongType(), True), StructField('diffmaglim', DoubleType(), True), StructField('pdiffimfilename', StringType(), True), StructField('programpi', StringType(), True), StructField('programid', LongType(), True), StructField('candid', LongType(), True), StructField('isdiffpos', StringType(), True), StructField('tblid', LongType(), True), StructField('nid', LongType(), True), StructField('rcid', LongType(), True), StructField('field', LongType(), True), StructField('xpos', DoubleType(), True), StructField('ypos', DoubleType(), True), StructField('ra', DoubleType(), True), StructField('dec', DoubleType(), True), StructField('magpsf', DoubleType(), True), StructField('sigmapsf', DoubleType(), True), StructField('chipsf', DoubleType(), True), StructField('magap', DoubleType(), True), StructField('sigmagap', DoubleType(), True), StructField('distnr', DoubleType(), True), StructField('magnr', DoubleType(), True), StructField('sigmagnr', DoubleType(), True), StructField('chinr', DoubleType(), True), StructField('sharpnr', DoubleType(), True), StructField('sky', DoubleType(), True), StructField('magdiff', DoubleType(), True), StructField('fwhm', DoubleType(), True), StructField('classtar', DoubleType(), True), StructField('mindtoedge', DoubleType(), True), StructField('magfromlim', DoubleType(), True), StructField('seeratio', DoubleType(), True), StructField('aimage', DoubleType(), True), StructField('bimage', DoubleType(), True), StructField('aimagerat', DoubleType(), True), StructField('bimagerat', DoubleType(), True), StructField('elong', DoubleType(), True), StructField('nneg', LongType(), True), StructField('nbad', LongType(), True), StructField('rb', DoubleType(), True), StructField('ssdistnr', DoubleType(), True), StructField('ssmagnr', DoubleType(), True), StructField('ssnamenr', StringType(), True), StructField('sumrat', DoubleType(), True), StructField('magapbig', DoubleType(), True), StructField('sigmagapbig', DoubleType(), True), StructField('ranr', DoubleType(), True), StructField('decnr', DoubleType(), True), StructField('sgmag1', DoubleType(), True), StructField('srmag1', DoubleType(), True), StructField('simag1', DoubleType(), True), StructField('szmag1', DoubleType(), True), StructField('sgscore1', DoubleType(), True), StructField('distpsnr1', DoubleType(), True), StructField('ndethist', LongType(), True), StructField('ncovhist', LongType(), True), StructField('jdstarthist', DoubleType(), True), StructField('jdendhist', DoubleType(), True), StructField('scorr', DoubleType(), True), StructField('tooflag', LongType(), True), StructField('objectidps1', LongType(), True), StructField('objectidps2', LongType(), True), StructField('sgmag2', DoubleType(), True), StructField('srmag2', DoubleType(), True), StructField('simag2', DoubleType(), True), StructField('szmag2', DoubleType(), True), StructField('sgscore2', DoubleType(), True), StructField('distpsnr2', DoubleType(), True), StructField('objectidps3', LongType(), True), StructField('sgmag3', DoubleType(), True), StructField('srmag3', DoubleType(), True), StructField('simag3', DoubleType(), True), StructField('szmag3', DoubleType(), True), StructField('sgscore3', DoubleType(), True), StructField('distpsnr3', DoubleType(), True), StructField('nmtchps', LongType(), True), StructField('rfid', LongType(), True), StructField('jdstartref', DoubleType(), True), StructField('jdendref', DoubleType(), True), StructField('nframesref', LongType(), True), StructField('rbversion', StringType(), True), StructField('dsnrms', DoubleType(), True), StructField('ssnrms', DoubleType(), True), StructField('dsdiff', DoubleType(), True), StructField('magzpsci', DoubleType(), True), StructField('magzpsciunc', DoubleType(), True), StructField('magzpscirms', DoubleType(), True), StructField('nmatches', LongType(), True), StructField('clrcoeff', DoubleType(), True), StructField('clrcounc', DoubleType(), True), StructField('zpclrcov', DoubleType(), True), StructField('zpmed', DoubleType(), True), StructField('clrmed', DoubleType(), True), StructField('clrrms', DoubleType(), True), StructField('neargaia', DoubleType(), True), StructField('neargaiabright', DoubleType(), True), StructField('maggaia', DoubleType(), True), StructField('maggaiabright', DoubleType(), True), StructField('exptime', DoubleType(), True), StructField('drb', DoubleType(), True), StructField('drbversion', StringType(), True)]), True), StructField('prv_candidates', ArrayType(StructType([StructField('jd', DoubleType(), True), StructField('fid', LongType(), True), StructField('pid', LongType(), True), StructField('diffmaglim', DoubleType(), True), StructField('pdiffimfilename', StringType(), True), StructField('programpi', StringType(), True), StructField('programid', LongType(), True), StructField('candid', LongType(), True), StructField('isdiffpos', StringType(), True), StructField('tblid', LongType(), True), StructField('nid', LongType(), True), StructField('rcid', LongType(), True), StructField('field', LongType(), True), StructField('xpos', DoubleType(), True), StructField('ypos', DoubleType(), True), StructField('ra', DoubleType(), True), StructField('dec', DoubleType(), True), StructField('magpsf', DoubleType(), True), StructField('sigmapsf', DoubleType(), True), StructField('chipsf', DoubleType(), True), StructField('magap', DoubleType(), True), StructField('sigmagap', DoubleType(), True), StructField('distnr', DoubleType(), True), StructField('magnr', DoubleType(), True), StructField('sigmagnr', DoubleType(), True), StructField('chinr', DoubleType(), True), StructField('sharpnr', DoubleType(), True), StructField('sky', DoubleType(), True), StructField('magdiff', DoubleType(), True), StructField('fwhm', DoubleType(), True), StructField('classtar', DoubleType(), True), StructField('mindtoedge', DoubleType(), True), StructField('magfromlim', DoubleType(), True), StructField('seeratio', DoubleType(), True), StructField('aimage', DoubleType(), True), StructField('bimage', DoubleType(), True), StructField('aimagerat', DoubleType(), True), StructField('bimagerat', DoubleType(), True), StructField('elong', DoubleType(), True), StructField('nneg', LongType(), True), StructField('nbad', LongType(), True), StructField('rb', DoubleType(), True), StructField('ssdistnr', DoubleType(), nullable=True), StructField('ssmagnr', DoubleType(), True), StructField('ssnamenr', StringType(), True), StructField('sumrat', DoubleType(), True), StructField('magapbig', DoubleType(), True), StructField('sigmagapbig', DoubleType(), True), StructField('ranr', DoubleType(), True), StructField('decnr', DoubleType(), True), StructField('scorr', DoubleType(), True), StructField('magzpsci', DoubleType(), True), StructField('magzpsciunc', DoubleType(), True), StructField('magzpscirms', DoubleType(), True), StructField('clrcoeff', DoubleType(), True), StructField('clrcounc', DoubleType(), True), StructField('rbversion', StringType(), True)]), True), True), StructField('fp_hists', ArrayType(StructType([StructField('field', LongType(), True), StructField('rcid', LongType(), True), StructField('fid', LongType(), True), StructField('pid', LongType(), True), StructField('rfid', LongType(), True), StructField('sciinpseeing', DoubleType(), True), StructField('scibckgnd', DoubleType(), True), StructField('scisigpix', DoubleType(), True), StructField('magzpsci', DoubleType(), True), StructField('magzpsciunc', DoubleType(), True), StructField('magzpscirms', DoubleType(), True), StructField('clrcoeff', DoubleType(), True), StructField('clrcounc', DoubleType(), True), StructField('exptime', DoubleType(), True), StructField('adpctdif1', DoubleType(), True), StructField('adpctdif2', DoubleType(), True), StructField('diffmaglim', DoubleType(), True), StructField('programid', LongType(), True), StructField('jd', DoubleType(), True), StructField('forcediffimflux', DoubleType(), True), StructField('forcediffimfluxunc', DoubleType(), True), StructField('procstatus', StringType(), True), StructField('distnr', DoubleType(), True), StructField('ranr', DoubleType(), True), StructField('decnr', DoubleType(), True), StructField('magnr', DoubleType(), True), StructField('sigmagnr', DoubleType(), True), StructField('chinr', DoubleType(), True), StructField('sharpnr', DoubleType(), True)]), True), True)])
 
 
@@ -241,11 +242,36 @@ def _apply_transformations(df):
         F.when(F.col("unparsed_fid") == 1, "g").when(F.col("unparsed_fid") == 2, "r").otherwise("i").alias("fid"),
         (F.col("jd") - 2400000.5).alias("mjd"),
         F.col("isdiffpos").alias("unparsed_isdiffpos"),
-        F.when(F.col("fid") == 1, 0.065).when(F.col("fid") == 2, 0.085).otherwise(0.01).alias("e_dec"),
-        F.when(F.cos(F.radians(F.col("dec"))) != 0, F.col("e_dec") / F.abs(F.cos(F.radians(F.col("dec"))))).otherwise(float('nan')).alias("e_ra")
+        F.when(F.col("fid") == 1, 0.065).when(F.col("fid") == 2, 0.085).otherwise(0.01).alias("e_dec_decimal"),
+        F.when(F.col("fid") == 1, 0.06499999761581421).when(F.col("fid") == 2, 0.08500000089406967).otherwise(0.01).alias("e_dec"),
+        F.when(F.cos(F.radians(F.col("dec"))) != 0, F.col("e_dec_decimal") / F.abs(F.cos(F.radians(F.col("dec"))))).otherwise(float('nan')).alias("e_ra")
     )
+    df = df.drop('e_dec_decimal')
+
+    import numpy
+    from pyspark.sql.types import FloatType
+    def format_and_convert(value):
+        # Convert the value to numpy float32
+        float32_value = numpy.float32(value)
+        # Format to string with high precision (24 decimal places)
+        formatted_value = format(float32_value, '.24f')
+        # Convert the formatted string back to float
+        return float(formatted_value)
+
+    # Register the function as a UDF
+    format_and_convert_udf = udf(format_and_convert, FloatType())
+
+    # Apply the UDF to the 'e_ra' column
+    df= df.withColumn("e_ra", format_and_convert_udf(df["e_ra"]))
+
+# 
+
+
+
+
     return df
 
+#! missing true value of 0.01
 
 ############################### SORTING HAT AID GENERATION ##############################################
 def alerce_id_generator(df):
@@ -255,15 +281,15 @@ def alerce_id_generator(df):
         when(col("ra") < 0, col("ra") + 360)
             .when(col("ra") > 360, col("ra") - 360)
             .otherwise(col("ra")).alias("ra_fixed")
-    ).drop("ra").withColumnRenamed("ra_fixed", "ra")
+    )
 
     # Calculate Ra components
     df = df.select(
         "*",
-        ((col("ra") / 15).cast("bigint")).alias("ra_hh"),
-        (((col("ra") / 15) - ((col("ra") / 15).cast("bigint"))) * 60).cast("bigint").alias("ra_mm"),
-        ((((col("ra") / 15) - ((col("ra") / 15).cast("bigint"))) * 60 - ((((col("ra") / 15) - ((col("ra") / 15).cast("bigint"))) * 60).cast("bigint"))) * 60).cast("bigint").alias("ra_ss"),
-        (((((col("ra") / 15) - ((col("ra") / 15).cast("bigint"))) * 60 - ((((col("ra") / 15) - ((col("ra") / 15).cast("bigint"))) * 60).cast("bigint"))) * 60 - (((((col("ra") / 15) - ((col("ra") / 15).cast("bigint"))) * 60 - ((((col("ra") / 15) - ((col("ra") / 15).cast("bigint"))) * 60).cast("bigint"))) * 60).cast("bigint"))).alias("ra_ff")
+        ((col("ra_fixed") / 15).cast("bigint")).alias("ra_hh"),
+        (((col("ra_fixed") / 15) - ((col("ra_fixed") / 15).cast("bigint"))) * 60).cast("bigint").alias("ra_mm"),
+        ((((col("ra_fixed") / 15) - ((col("ra_fixed") / 15).cast("bigint"))) * 60 - ((((col("ra_fixed") / 15) - ((col("ra_fixed") / 15).cast("bigint"))) * 60).cast("bigint"))) * 60).cast("bigint").alias("ra_ss"),
+        (((((col("ra_fixed") / 15) - ((col("ra_fixed") / 15).cast("bigint"))) * 60 - ((((col("ra_fixed") / 15) - ((col("ra_fixed") / 15).cast("bigint"))) * 60).cast("bigint"))) * 60 - (((((col("ra_fixed") / 15) - ((col("ra_fixed") / 15).cast("bigint"))) * 60 - ((((col("ra_fixed") / 15) - ((col("ra_fixed") / 15).cast("bigint"))) * 60).cast("bigint"))) * 60).cast("bigint"))).alias("ra_ff")
     ))
 
     # Fix negative Dec
@@ -271,16 +297,16 @@ def alerce_id_generator(df):
         "*",
         when(col("dec") >= 0, lit(1)).otherwise(lit(0)).alias("h"),
         abs(col("dec")).alias("dec_fixed")
-    ).drop("dec").withColumnRenamed("dec_fixed", "dec")
+    )
 
     # Calculate Dec components
     # Calculate Dec components
     df = df.select(
         "*",
-    (col("dec") / 15).cast("bigint").alias("dec_deg"),
-    (((col("dec") / 15) - (col("dec") / 15).cast("bigint")) * 60).cast("bigint").alias("dec_mm"),
-    (((((col("dec") / 15) - (col("dec") / 15).cast("bigint")) * 60) - (((col("dec") / 15) - (col("dec") / 15).cast("bigint")) * 60).cast("bigint")) * 60).cast("bigint").alias("dec_ss"),
-    ((((((col("dec") / 15) - (col("dec") / 15).cast("bigint")) * 60) - (((col("dec") / 15) - (col("dec") / 15).cast("bigint")) * 60).cast("bigint")) * 60) - ((((((col("dec") / 15) - (col("dec") / 15).cast("bigint")) * 60) - (((col("dec") / 15) - (col("dec") / 15).cast("bigint")) * 60).cast("bigint")) * 60).cast("bigint"))).alias("dec_f")
+    (col("dec_fixed") / 15).cast("bigint").alias("dec_deg"),
+    (((col("dec_fixed") / 15) - (col("dec_fixed") / 15).cast("bigint")) * 60).cast("bigint").alias("dec_mm"),
+    (((((col("dec_fixed") / 15) - (col("dec_fixed") / 15).cast("bigint")) * 60) - (((col("dec_fixed") / 15) - (col("dec_fixed") / 15).cast("bigint")) * 60).cast("bigint")) * 60).cast("bigint").alias("dec_ss"),
+    ((((((col("dec_fixed") / 15) - (col("dec_fixed") / 15).cast("bigint")) * 60) - (((col("dec_fixed") / 15) - (col("dec_fixed") / 15).cast("bigint")) * 60).cast("bigint")) * 60) - ((((((col("dec_fixed") / 15) - (col("dec_fixed") / 15).cast("bigint")) * 60) - (((col("dec_fixed") / 15) - (col("dec_fixed") / 15).cast("bigint")) * 60).cast("bigint")) * 60).cast("bigint"))).alias("dec_f")
     )
 
     # Calculate the aid
@@ -299,7 +325,7 @@ def alerce_id_generator(df):
     )
 
     # Drop intermediate columns
-    df = df.drop("ra_hh", "ra_mm", "ra_ss", "ra_ff", "h", "dec_deg", "dec_mm", "dec_ss", "dec_f")
+    df = df.drop("ra_hh", "ra_mm", "ra_ss", "ra_ff", "ra_fixed","h", "dec_deg", "dec_mm", "dec_ss", "dec_f", "dec_fixed")
     
     return df
 