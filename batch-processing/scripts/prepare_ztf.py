dd7e99b38ea14942484dd01929733524a6c82d05
kaymedinam <kaymedinam@gmail.com>
2024-08-12 15:12:43 -0400

    Clean out dated scripts
    Former-commit-id: 6035ef1b8786ca63030b1b89c28d97917920dd9c

--- batch-processing/scripts/prepare_ztf.py
+++ batch-processing/scripts/prepare_ztf.py
@@ -1,9 +1,8 @@
-from pyspark.sql import SparkSession
 from batch_processing.ztf import ZTFCrawler
 from performance_timer import PerformanceTimer
 
 with PerformanceTimer("[SETUP] initialize ZTFCrawler"):
-    config_path = "/home/edipizarro/projects/batch_processing/batch_processing/configs/raw.config.json"
+    config_path = "/home/user/projects/batch_processing/batch_processing/configs/raw.config.json"
     ztf = ZTFCrawler(config_path)
 
 with PerformanceTimer("[ZTFCrawler] execute"):
