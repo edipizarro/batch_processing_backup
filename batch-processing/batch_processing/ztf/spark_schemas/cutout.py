1ce7ee33224e31900ed65c7ae83f34fafc2c9be6
kaymedinam <kaymedinam@gmail.com>
2024-08-12 15:09:18 -0400

    Clean code that processes ztf avros

--- batch-processing/batch_processing/ztf/spark_schemas/cutout.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql.types import StructType, StructField, StringType, BinaryType
-
-cutout_schema = StructType([
-    StructField("fileName", StringType(), True),
-    StructField("stampData", BinaryType(), True)
-])
