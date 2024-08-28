9e2278ca8a1d4bd0d0df4f0f1225d77ac270aa46
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2024-08-28 11:50:12 -0400

    refactor: Import steps from files
    Remove __init__.py files from steps folders as to import them directly
    from the .py file instead of the folder.
    
    
    Former-commit-id: 6319d7f586621086d04d3355936f55a686983a29

--- batch-processing/batch_processing/pipeline/__init__.py
+++ batch-processing/batch_processing/pipeline/__init__.py
@@ -1,7 +1,9 @@
+from .api import API
 from .correction_batch import correction_step_pyspark as correction
 from .create_dataframes import create_dets_nds_phots_dataframes as create_dataframes
-from .magstats import magstats_pyspark as magstats
-from .sorting_hat import sorting_hat_spark as sorting_hat
-from .prv_candidates import prv_candidates_pyspark as prv_candidates
 from .lightcurve_batch import light_curve_step_pyspark as lightcurve
+from .magstats import magstats_pyspark as magstats
+from .prv_candidates import prv_candidates_pyspark as prv_candidates
+from .sorting_hat import sorting_hat_spark as sorting_hat
+from .spark_init import pyspark_configs
 from .xmatch import xmatch_step_pyspark_refactor as xmatch
