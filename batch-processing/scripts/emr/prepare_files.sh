9b13e104f1fbcbe93d57a56f728f1458d3588e21
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2024-08-28 13:54:41 -0400

    docs: Example script to create files for EMR cluster
    Added script that helps run batch processing in the cluster easily
    creating the packages necessary for spark-submit.

--- batch-processing/scripts/emr/prepare_files.sh
+++ batch-processing/scripts/emr/prepare_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check input
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <base_path>"
+  exit 1
+fi
+
 # Define variables
 BASE_DIR=$1
 DIST_DIR="${BASE_DIR}/dist"
@@ -7,30 +13,39 @@ VENV_PATH=$(python -m poetry run which python | sed 's|/bin/python||')
 SITE_PACKAGES="${VENV_PATH}/lib/python3.9/site-packages"
 SITE_PACKAGES64="${VENV_PATH}/lib64/python3.9/site-packages"
 
-# Change to the specified directory
-cd "${BASE_DIR}" || { echo "Failed to change directory to ${BASE_DIR}"; exit 1; }
+# Function to prepare Python dependencies with Poetry
+prepare_poetry() {
+  cd "${BASE_DIR}" || { echo "Failed to change directory to ${BASE_DIR}"; exit 1; }
+  python -m poetry lock || { echo "Failed to create lock file."; exit 1; }
+  python -m poetry install || { echo "Failed to install Poetry dependencies."; exit 1; }
+  python -m build || { echo "Failed to build Poetry project."; exit 1; }
+}
 
-# Run poetry commands
-python -m poetry lock
-python -m poetry install
-python -m poetry build
+# Function to create a zip file with the content of a specified folder
+zip_folder_content() {
+  origin=$1
+  destination=$2
+  filename=$3
+  cd "${origin}" || { echo "Failed to change directory to ${VENV_PATH}"; exit 1; }
+  zip -r "${DIST_DIR}/python-venv.zip" . || { echo "Failed to zip ${VENV_PATH}"; exit 1; }
+}
 
-# Get Python executable path from poetry
-PYTHON_PATH=$(python -m poetry run which python)
+# Prepare files for Spark
+prepare_venv() {
+  # Get Python executable path from poetry
+  PYTHON_PATH=$(python -m poetry run which python)
 
-# Install the built wheel file
-${PYTHON_PATH} -m pip install "${DIST_DIR}/batch_processing-0.1.0-py3-none-any.whl"
+  # Install built wheel explictly
+  ${PYTHON_PATH} -m pip install "${DIST_DIR}/batch_processing-0.1.0-py3-none-any.whl"
 
-## Zip site-packages
-#cd "${SITE_PACKAGES}" || { echo "Failed to change directory to ${SITE_PACKAGES}"; exit 1; }
-#zip -r "${DIST_DIR}/site-packages.zip" . || { echo "Failed to zip ${SITE_PACKAGES}"; exit 1; }
+  # Zip python-venv
+  zip_folder_content ${VENV_PATH} ${DIST_DIR} python-venv.zip
+}
 
-## Zip site-packages64
-#cd "${SITE_PACKAGES64}" || { echo "Failed to change directory to ${SITE_PACKAGES64}"; exit 1; }
-#zip -r "${DIST_DIR}/site-packages64.zip" . || { echo "Failed to zip ${SITE_PACKAGES64}"; exit 1; }
+main() {
+  prepare_poetry
+  prepare_venv
+}
 
-# Zip python-venv
-cd "${VENV_PATH}" || { echo "Failed to change directory to ${VENV_PATH}"; exit 1; }
-zip -r "${DIST_DIR}/python-venv.zip" . || { echo "Failed to zip ${VENV_PATH}"; exit 1; }
-
+main
 echo "Script completed successfully."
