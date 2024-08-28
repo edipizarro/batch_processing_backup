3f074f912b6b835521469e4fb19f28db5e4e7182
Edison Pizarro <edison.pizarro.godoy@gmail.com>
2024-08-28 13:58:34 -0400

    docs: scripts to create initial parquet files
    Example scripts to create initial parquet files.
    It includes:
     - download tar files
     - uncompress avro files
     - convert avro to parquet
    
    
    Former-commit-id: 93d3f3c68bc873bf86bf46f2102a62b9dd559970

--- batch-processing/scripts/prepare_ztf.py
+++ batch-processing/scripts/prepare_ztf.py
@@ -1,9 +1,48 @@
-from batch_processing.ztf import ZTFCrawler
+import argparse
+
 from performance_timer import PerformanceTimer
 
+from batch_processing.ztf import ZTFCrawler
+
+with PerformanceTimer("[SETUP] Parse arguments"):
+    parser = argparse.ArgumentParser(
+        description="Script to convert folders with AVRO files to PARQUET files"
+    )
+
+    parser.add_argument("--mjd", type=str, help="MJD", required=True)
+
+    parser.add_argument(
+        "-i",
+        "--input-avro-folder",
+        type=str,
+        help="Path where input avro files are located",
+        required=True,
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output-parquet-folder",
+        type=str,
+        help="Path where output parquet files will be stored",
+        required=True,
+    )
+
+    parser.add_argument(
+        "-s",
+        "--batch-size",
+        type=str,
+        help="AVRO alerts per parquet file",
+        default=50000,
+    )
+    args = parser.parse_args()
+
 with PerformanceTimer("[SETUP] initialize ZTFCrawler"):
-    config_path = "/home/user/projects/batch_processing/batch_processing/configs/raw.config.json"
-    ztf = ZTFCrawler(config_path)
+    ztf = ZTFCrawler(
+        args.mjd,
+        args.input_avro_folder,
+        args.output_parquet_folder,
+        int(args.batch_size),
+    )
 
 with PerformanceTimer("[ZTFCrawler] execute"):
     ztf.execute()
